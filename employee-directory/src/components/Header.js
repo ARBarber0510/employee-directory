501fc08a679e3a7a717b67f0f2e034f6ea270cba
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-11-17 07:22:30 -0600

    added header code to js file

---
+++ employee-directory/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "../styles/Header.css";
+
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>Employee Directory</h1>
+                <p>Use search box to narrow your results or filter by heading using the arrows</p>
+            </div>
+        )
+    }
+};
