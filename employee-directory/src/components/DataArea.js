556173954c3b59d7cb37043b389454d6aa1aaf45
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-11-18 21:59:42 -0600

    updated code for DataArea.js

--- employee-directory/src/components/DataArea.js
+++ employee-directory/src/components/DataArea.js
@@ -30,5 +30,66 @@ export default class DataArea extends Component {
         })
        }
 
+    const compareFnc = (a, b) => {
+        if (this.state.order === "ascend") {
+            if (a[heading] === undefined) {
+                return 1;
+            } else if (b[heading] === undefined) {
+                return -1;
+            }
+            else if (heading === "name") {
+                return a[heading].first.localeCompare(b[heading].first);
+            } else {
+                return a[heading] - b[heading];
+            }
+        } else {
+            if (a[heading] === undefined) {
+                return 1;
+            } else if (b[heading] === undefined) {
+                return -1;
+            }
+            else if (heading === "name") {
+                return b[heading].first.localeCompare(a[heading].first);
+            } else {
+                return b[heading] - a[heading];
+            }
+        }
+    }
+    const sortedUsers = this.state.filteredUsers.sort(compareFnc);
+    this.setState({ filteredUsers: sortedUsers});
+   }
+
+   handleSearchChange = event => {
+       const filter = event.target.value;
+       const filteredList = this.state.users.filter(item => {
+           let values = Object.values(item)
+                .join("")
+                .toLowerCase();
+            return values.indexOf(filter.toLowerCase()) !== -1;
+       });
+       this.setState({ filteredUsers: filteredList});
+   }
+
+   componentDidMount() {
+       API.getUsers().then(results => {
+           this.setState({
+               users: results.data.results
+           });
+       });
+   }
+
+   render() {
+       return (
+           <>
+           <Nav handleSearchChange={this.handleSearchChange} />
+                <div className="data-area">
+                    <DataTable
+                    headings={this.headings}
+                    users={this.state.filteredUsers}
+                    handleSort={this.handleSort}
+                    />
+                </div>
+            </>
+       );
    }
 }
